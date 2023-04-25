77833204124cf90d07de28d2af3f94ea48c0e1cc
smdirfan72 <123714668+smdirfan72@users.noreply.github.com>
2023-04-25 13:44:46 +0530

    Create 25-04 Q7.c

---
+++ 25-04 Q7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j, k;
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array in ascending order:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n;) {
+            if (arr[j] == arr[i]) {
+                for (k = j; k < n; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                n--;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    printf("Array after removing duplicates:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
